84f87f15b30ebaac2e88bbb2397205fa194326c3
NechiCompany001 <ealdiar99@gmail.com>
2026-01-16 11:36:45 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById("btn");
+const btn2 = document.getElementById("btn2");
 
 btn.addEventListener("click", function() {
   alert("Value: 1");
+})
+
+btn2.addEventListener("click", function() {
+  alert("Value: 2")
 })
 
